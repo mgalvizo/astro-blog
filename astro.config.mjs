323eff0c66cc9c37b2bc7fd0a91b56ab627851e7
Miguel Garcia <miguelangelgarciaalvizo@hotmail.com>
2025-01-21 17:21:39 -0600

    add netlify adapter for on demand pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,16 @@ import { defineConfig } from "astro/config";
 // import preact from "@astrojs/preact";
 import react from "@astrojs/react";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
+
   integrations: [
     // preact(),
     react(),
   ],
+
+  adapter: netlify(),
 });
